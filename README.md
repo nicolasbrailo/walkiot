478a6125a19cf8b81d55dfa14db55e08aa7d129d
Nico <nicolasbrailo@gmail.com>
2018-11-25 15:44:59 +0100

    Added sounds

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # walkiot
 Absurdly overengineered baby toy with a Raspberry Pi
+
+
+Hola https://freesound.org/people/upfusername/sounds/393450/
+Cow http://www.findsounds.com/animals.html
+Phone ring https://freesound.org/people/cs272/sounds/77723/
+Weee https://freesound.org/people/scuzzpuck/sounds/40838/
+Tada https://freesound.org/people/usinggarageband/sounds/152574/
+Numbers https://freesound.org/people/sergeeo/sounds/177270/
+
+
--- src/walkiot.py
+++ src/walkiot.py
@@ -1,11 +1,12 @@
 import pygame
 pygame.mixer.init()
 
-for i in range(pygame.mixer.get_num_channels()):
-    c = pygame.mixer.Channel(i)
-    c.set_volume(1)
+# for i in range(pygame.mixer.get_num_channels()):
+#     c = pygame.mixer.Channel(i)
+#     c.set_volume(1)
 
 def sync_play_sound(fn):
+    print("Play " + fn)
     pygame.mixer.music.load(fn)
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy() == True:
@@ -26,8 +27,29 @@ def connect_to_button(pin, cb):
 
 
 class KeySound(object):
-    def __init__(self):
-        self.keys = {
+    def _book_keys(self):
+        return {
+           # Piano
+            5: "sounds/nums/1.wav",
+            6: "sounds/nums/2.wav",
+           13: "sounds/nums/3.wav",
+           26: "sounds/nums/4.wav",
+            0: "sounds/nums/5.wav",
+
+           # Cow
+           11: "sounds/cow.wav",
+           # Phone
+           9: ["sounds/phone.wav", "sounds/hola.wav"],
+           # Rolling thingy
+           22: "sounds/weee.wav",
+           # Sun button
+           27: "sounds/hola.wav",
+           # Triangle button
+           1: "sounds/tada.wav",
+        }
+
+    def _music_keys(self):
+        return {
            # Piano
             5: "sounds/piano/do.wav",
             6: "sounds/piano/re.wav",
@@ -36,27 +58,40 @@ class KeySound(object):
             0: "sounds/piano/sol.wav",
 
            # Cow
-           11: "i.wav",
+           11: "sounds/cow.wav",
            # Phone
-           9: "i.wav",
-           # Switch book/music?
-           10: "i.wav",
+           9: ["sounds/phone.wav", "sounds/hola.wav"],
            # Rolling thingy
-           22: "i.wav",
+           22: "sounds/weee.wav",
            # Sun button
-           27: "i.wav",
-           # Square button
-           16: "i.wav",
+           27: "sounds/hola.wav",
            # Triangle button
-           1: "i.wav",
+           1: "sounds/tada.wav",
         }
 
+    def __init__(self):
+        self._book_music_switch = Button(10)
+        # Pins are the same for both modes, arbitrarily pick one
+        pins = self._music_keys()
         self._btns = [connect_to_button(pin, self.on_key_press)
-                        for pin in self.keys.keys()]
+                        for pin in pins.keys()]
+
+    def book_mode_enabled(self):
+        return self._book_music_switch.is_active
+
+    def _active_pin_map(self):
+        if self.book_mode_enabled():
+            return self._book_keys()
+        else:
+            return self._music_keys()
 
     def on_key_press(self, pin):
-        print("Play " + self.keys[pin])
-        sync_play_sound(self.keys[pin])
+        pinmap = self._active_pin_map()
+        if isinstance(pinmap, (list,)):
+            for snd in pinmap:
+                sync_play_sound(snd)
+        else:
+            sync_play_sound(pinmap)
 
 foo = KeySound()
 while True:
